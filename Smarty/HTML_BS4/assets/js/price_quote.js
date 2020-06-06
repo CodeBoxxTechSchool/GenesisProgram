4cc839e8776763eee9f04dfcdba992a729b6e175
Herbert Magramo <hrmagramo@ymail.com>
2020-06-06 10:11:07 -0400

    price estimation form

---
+++ Smarty/HTML_BS4/assets/js/price_quote.js
@@ -0,0 +1,60 @@
+/** ********************************************** **
+	@Author			Herbert Magramo
+	@Website		info.codebox.biz
+	@Last Update	Thursay, February 22, 2018
+
+	NOTE! 	Price quotation for Rocket Elevators
+*************************************************** **/
+
+
+
+/** ********** For residential buildings computation ************** **
+  Variables: avgFloors = number of apartments / number of floors
+             (Note: if the number of floors > 20 add 1 column)
+             totalElevators = avgFloors /6
+             costOfElevators = radio button selection * totalElevators
+             installationFee = elevatorPrice * radio button selection
+             totalPrice = costOfElevators + installationFee
+******************************************************************* **/
+
+
+function get
+
+
+
+
+
+/** ************* For commercial buildings computation ****************** **
+  Variables: costOfElevators = numberOfElevators * radio button selection
+             installationFee = elevatorPrice * radio button selection
+             totalPrice = costOfElevators + installationFee
+************************************************************************* **/
+
+
+
+
+
+
+/** ********** For corporate buildings computation ************** **
+  Variables: totalNumFloors = numFloors + numBasements
+             avgOccupants = maxOccupants + totalNumFloors
+             totalElevators = avgOccupants / 1000
+             costOfElevators = radio button selection * totalElevators
+             installationFee = elevatorPrice * radio button selection
+             totalPrice = costOfElevators + installationFee
+****************************************************************** **/
+
+
+
+
+
+
+/** ********** For hybrid buildings computation ************** **
+  Variables: totalNumFloors = numFloors + numBasements
+             avgOccupants = maxOccupants + totalNumFloors
+             totalElevators = avgOccupants / 1000
+             costOfElevators = radio button selection * totalElevators
+             installationFee = elevatorPrice * radio button selection
+             totalPrice = costOfElevators + installationFee
+************************************************************** **/
+
