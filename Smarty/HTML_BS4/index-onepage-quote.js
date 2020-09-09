95fd038d3e91c9abba4372201054fc5b84886cd2
emilie desaulniers <70654862+emiliedesaulniers@users.noreply.github.com>
2020-09-08 21:11:44 -0400

    day 6 of kms (2.0)

--- Smarty/HTML_BS4/index-onepage-quote.js
+++ Smarty/HTML_BS4/index-onepage-quote.js
@@ -1,1 +1,27 @@
-//XD
+Qualtrics.SurveyEngine.addOnload(function ()
+{
+document.getElementById("QID8").style.display="none";
+
+this.questionclick = function(event,element)
+{
+    console.log(event, element);
+    if (element.type == 'radio')
+    {
+        var choiceNum = element.id.split('~')[2];
+
+        var x = document.getElementById("QID2") //Question ID of the textbox Q
+    x.style.display="none"
+
+    this.questionclick = function(event, element) {
+        var selectedChoice = this.getSelectedChoices()
+
+        console.log(selectedChoice) //use this to get the value of the choice when you want the textbox to appear
+        if (selectedChoice == "1") {
+            x.style.display="block"
+        }
+        else {
+            x.style.display="none"
+        }
+    }
+}
+});
