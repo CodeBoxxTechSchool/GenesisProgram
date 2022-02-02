8ee04a5eacfa9ebb2d5ee5ca4ac9ec00545f6498
Eric <coolo_ricky@hotmail.com>
2022-02-02 12:30:44 -0500

    did some javascript. not working

--- Smarty/HTML_BS4/assets/js/quote.js
+++ Smarty/HTML_BS4/assets/js/quote.js
@@ -1,18 +1,81 @@
-var BUILDING_TYPE_RESIDENTIAL = "residential"
-var BUILDING_TYPE_COMMERCIAL = "commercial"
-var BUILDING_TYPE_CORPORATE = "corporate"
-var BUILDING_TYPE_HYBRID = "hybrid"
 
 
-function residentialBuilding("number-of-apartments", "number-of-floors", "number-of-basements") 
+var theForm = document.forms ["elevatorform"];
+
+var BUILDING_TYPE_RESIDENTIAL = "residential";
+var BUILDING_TYPE_COMMERCIAL = "commercial";
+var BUILDING_TYPE_CORPORATE = "corporate";
+var BUILDING_TYPE_HYBRID = "hybrid";
+
+var installationPrices = new Array ();
+Installation["Standard"]=20;
+Installation["Premium"]=25;
+Installation["Excelium"]=35;
+
+function getInstallationPrice()
 {
+    var installationPrice=0;
+    var theForm = document.forms["elevatorform"];
+    var selectedInstallation = theForm.elements["selectedInstallation"];
+    for (var i = 0; i < selectedInstallation.lenght; i++)
+    {
+        if(selectedInstallation[i].checked)
+        {
+        installationPrice = installationPrices[selectedInstallation[i].value];
+        break;
+    }
+ }    
+ return installationPrice
 }
-function commercialBuilding("number-of-floors", "number-of-basements", "number-of-companies", "number-of-parking-spots", "number-of-elevators") 
+
+function getTotal()
 {
+    var installationPrice = installationPrice;
+
+    //display the result
+    document.getElementById('installationPrice').innerHTML =
+                                      "Total Price For Installation $"+installationPrice;
+
 }
-function corporateBuilding("number-of-floors", "numbers-of-basements", "number-of-parking-spots", "number-of-corporations", "maximum-occupancy")
-{    
-}
-function hybridBuilding("number-of-floors", "numbers-of-basements", "number-of-companies", "number-of-parking-spots", "maximum-occupancy", "business-hours")
-{    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function residentialBuilding(number_of_apartments, number_of_floors, number_of_basements) 
+// {
+// }
+// function commercialBuilding(number_of_floors, number_of_basements, number_of_companies, number_of_parking_spots, number_of_elevators) 
+// {
+// }
+// function corporateBuilding(number_of_floors, numbers_of_basements, number_of_parking_spots, number_of_corporations, maximum_occupancy)
+// {    
+// }
+// function hybridBuilding(number_of_floors, numbers_of_basements, number_of_companies, number_of_parking_spots, maximum_occupancy, business_hours)
+// {    
+// }
+
+// //noms de variables, faire les 4 fonctions, fonction qui handle le show-hide (toggle), fonction de peser sur le bouton (laquelle est caller).
