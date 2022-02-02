ef87f6bd1985b0ed58fce7dfdc9de8eba9fdeb67
Goddraw <98338843+Goddraw@users.noreply.github.com>
2022-02-01 19:43:06 -0500

    full quote page html

---
+++ Smarty/HTML_BS4/assets/js/quote.js
@@ -0,0 +1,18 @@
+var BUILDING_TYPE_RESIDENTIAL = "residential"
+var BUILDING_TYPE_COMMERCIAL = "commercial"
+var BUILDING_TYPE_CORPORATE = "corporate"
+var BUILDING_TYPE_HYBRID = "hybrid"
+
+
+function residentialBuilding("number-of-apartments", "number-of-floors", "number-of-basements") 
+{
+}
+function commercialBuilding("number-of-floors", "number-of-basements", "number-of-companies", "number-of-parking-spots", "number-of-elevators") 
+{
+}
+function corporateBuilding("number-of-floors", "numbers-of-basements", "number-of-parking-spots", "number-of-corporations", "maximum-occupancy")
+{    
+}
+function hybridBuilding("number-of-floors", "numbers-of-basements", "number-of-companies", "number-of-parking-spots", "maximum-occupancy", "business-hours")
+{    
+}
